9cf4be45a2fbca1429a084297237b4b2d60a6e1c
Timmy <ccliu@net-chinese.com.tw>
2024-07-31 10:34:01 +0800

    update build file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'tmycafe.github.io/dist',
+  base: 'tmycafe.github.io',
   plugins: [
     vue(),
     vueDevTools(),
