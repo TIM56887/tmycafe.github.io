66e6bbb99633698ff2d9f63ecea3d03396103614
timmy <timmy56887@icloud.com>
2024-09-12 00:37:01 +0800

    chore:update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,22 @@
 // eslint.config.mjs
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+  },
+  {
+    // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+    files: ['**/*.vue'],
+    rules: {
+      'antfu/no-import-node-modules-by-path': 'off',
+    },
+  },
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      'style/semi': ['error', 'never'],
+    },
+  },
+)
